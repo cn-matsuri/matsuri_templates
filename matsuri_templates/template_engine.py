66b99cc7db596a5e1ce35ca83f33a2c3b85c13cd
FZxiao <42691214+fzxiao233@users.noreply.github.com>
2019-06-22 06:32:09 +0800

    Update template_engine.py

--- matsuri_templates/template_engine.py
+++ matsuri_templates/template_engine.py
@@ -2,5 +2,5 @@
 from jinja2 import Environment, PackageLoader
 env = Environment(loader=PackageLoader('twitter_template'))
 template = env.get_template('twitter.html')
-with open('./twitter_template/test.html', 'w+', encoding='utf-8') as f:
+with open('./twitter_template/template_store/test.html', 'w+', encoding='utf-8') as f:
     f.write(template.render(tweet_text='这里是推文内容', time='这里是发送时间', re='这里是转发次数', like='这里是喜欢次数', reply='这里是回复数'))
